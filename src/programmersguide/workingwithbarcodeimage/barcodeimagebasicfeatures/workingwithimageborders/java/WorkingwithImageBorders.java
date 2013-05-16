126e13b4aca5b8f6782786dd572130b53941942d
shoaibkhan-aspose <shoaib.khan@banckle.com>
2013-05-16 16:39:02 +0500

    Examples Added :
    - Working with Image Borders
    - Colorize any Part of the Barcode Image
    - Control Barcode Image Quality
    - Set Barcode Image Margins

---
+++ src/programmersguide/workingwithbarcodeimage/barcodeimagebasicfeatures/workingwithimageborders/java/WorkingwithImageBorders.java
@@ -0,0 +1,45 @@
+/* 
+ * Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+ *
+ * This file is part of Aspose.BarCode. The source code in this file
+ * is only intended as a supplement to the documentation, and is provided
+ * "as is", without warranty of any kind, either expressed or implied.
+ */
+ 
+package programmersguide.workingwithbarcodeimage.barcodeimagebasicfeatures.workingwithimageborders.java;
+
+import com.aspose.barcode.*;
+
+public class WorkingwithImageBorders
+{
+    public static void main(String[] args) throws Exception
+    {
+        // The path to the documents directory.
+        String dataDir = "src/programmersguide/workingwithbarcodeimage/barcodeimagebasicfeatures/workingwithimageborders/data/";
+      
+        //Instantiate barcode object
+        BarCodeBuilder bb = new BarCodeBuilder();
+
+        //Border will be visible
+        bb.setBorderVisible(true);
+
+        //Set border's width to be 0.5 millimeter
+        bb.setBorderWidth(0.5f);
+
+        //Set the border's color to red
+        bb.setBorderColor(java.awt.Color.RED);
+
+        try
+        {
+            //Save the image to file
+            bb.save(dataDir + "barcode-borders.out.jpg");
+            
+            //Print message
+            System.out.println("Barcode image generated successfully.");
+        }
+        catch (Exception ex)
+        {
+        	System.out.println("Some problem occurred while saving barcode image.");
+        }
+    }
+}
