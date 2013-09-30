a10d2f641899b869006a92aa16e2345ba86d3ce4
Fatima Ahmad <fatima.rabbiya@aspose.com>
2013-09-30 23:02:30 +0500

    Added New Examples
    Added New Examples

---
+++ src/programmersguide/workingwithbarcodeimage/barcodeimageutilityfeatures/generatebarcodecustomsize/java/GenerateBarcodeCustomSize.java
@@ -0,0 +1,43 @@
+/* 
+ * Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+ *
+ * This file is part of Aspose.BarCode. The source code in this file
+ * is only intended as a supplement to the documentation, and is provided
+ * "as is", without warranty of any kind, either expressed or implied.
+ */
+ 
+package programmersguide.workingwithbarcodeimage.barcodeimageutilityfeatures.generatebarcodecustomsize.java;
+
+import com.aspose.barcode.*;
+
+public class GenerateBarcodeCustomSize
+{
+    public static void main(String[] args) throws Exception
+    {
+        // The path to the documents directory.
+        String dataDir = "src/programmersguide/workingwithbarcodeimage/barcodeimageutilityfeatures/generatebarcodecustomsize/data/";
+
+        //Instantiate barcode object
+        BarCodeBuilder builder = new BarCodeBuilder("1234567890", Symbology.Code39Standard);
+
+        // Set auto size false
+        builder.setAutoSize(false);
+
+        // Set height
+        builder.setImageHeight(50);
+
+        // Set width
+        builder.setImageWidth(120);
+
+        // Save image to disk
+        builder.save(dataDir + "test.png");
+
+        // Print message
+        System.out.println("Barcode image generated successfully.");
+
+    }
+}
+
+
+
+
