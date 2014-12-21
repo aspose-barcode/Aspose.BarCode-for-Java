c9f4c773ad138405fb188e83e1827f50e158171f
FatimaAhmed <fatima.rabbiya@aspose.com>
2014-12-22 02:05:23 +0500

    New examples.

---
+++ src/programmersguide/workingwith2dbarcodes/2dbarcodesbasicfeatures/createaaztecbarcode/java/CreateAAztecBarcode.java
@@ -0,0 +1,26 @@
+/* 
+ * Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+ *
+ * This file is part of Aspose.BarCode. The source code in this file
+ * is only intended as a supplement to the documentation, and is provided
+ * "as is", without warranty of any kind, either expressed or implied.
+ */
+ 
+package programmersguide.workingwithbarcode.workingwith2dbarcodes.2dbarcodesbasicfeatures.createaaztecbarcode.java;
+
+import com.aspose.barcode.*;
+
+public class CreateAAztecBarcode
+{
+    public static void main(String[] args) throws Exception
+    {
+        // The path to the documents directory.
+        String dataDir = "src/programmersguide/workingwithbarcode/workingwith2dbarcodes/2dbarcodesbasicfeatures/createaaztecbarcode/data/";
+        
+        
+    }
+}
+
+
+
+
