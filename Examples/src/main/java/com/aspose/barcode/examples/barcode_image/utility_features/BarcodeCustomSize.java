1060e6cdcec00f5865c5d58abb7247d56ccea5e6
muzammil-khan <muzammil.khan@aspose.com>
2019-03-06 12:50:14 +0500

    Updated BarcodeCustomSize

--- Examples/src/main/java/com/aspose/barcode/examples/barcode_image/utility_features/BarcodeCustomSize.java
+++ Examples/src/main/java/com/aspose/barcode/examples/barcode_image/utility_features/BarcodeCustomSize.java
@@ -1,34 +1,31 @@
 package com.aspose.barcode.examples.barcode_image.utility_features;
 
-import com.aspose.barcode.BarCodeBuilder;
+import com.aspose.barcode.generation.AutoSizeMode;
+import com.aspose.barcode.generation.BarCodeGenerator;
 import com.aspose.barcode.examples.Utils;
 
+import java.io.IOException;
+
 public class BarcodeCustomSize {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
     	
     	// The path to the resource directory.
     	String dataDir = Utils.getDataDir(BarcodeCustomSize.class) + "BarcodeImage/UtilityFeatures/";
 
-        //Instantiate barcode object
-        BarCodeBuilder builder = new BarCodeBuilder();
-
-        //Set the Code text for the barcode
-        builder.setCodeText("1234567890");
-
-        //Set the symbology type to Code39Standard
-        builder.setEncodeType(com.aspose.barcode.EncodeTypes.CODE_39_STANDARD);
+        //Instantiate barcode object, Set the Code text for the barcode and the symbology type to Code39Standard
+        BarCodeGenerator generator = new BarCodeGenerator(com.aspose.barcode.EncodeTypes.CODE_39_STANDARD, "1234567890");
 
         // Set auto size false
-        builder.setAutoSize(false);
+        generator.setAutoSizeMode(AutoSizeMode.NEAREST);
 
         // Set height
-        builder.setImageHeight(50);
+        generator.getBarCodeHeight().setMillimeters(50);
 
         // Set width
-        builder.setImageWidth(120);
+        generator.getBarCodeWidth().setMillimeters(120);
 
         // Save the image
-        builder.save(dataDir + "barcode-custom-size.jpg");
+        generator.save(dataDir + "barcode-custom-size.jpg");
         
     }
 }
