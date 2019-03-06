c8ca15fac13bfe6f19fedb11fb537ea5ba489f98
unknown <aspose-com-gists@aspose.com>
2019-03-06 12:28:27 +0500

    Updated SaveBarcodeImageToStreams

--- Examples/src/main/java/com/aspose/barcode/examples/barcode_image/utility_features/SaveBarcodeImageToStreams.java
+++ Examples/src/main/java/com/aspose/barcode/examples/barcode_image/utility_features/SaveBarcodeImageToStreams.java
@@ -3,19 +3,16 @@ package com.aspose.barcode.examples.barcode_image.utility_features;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import com.aspose.barcode.BarCodeBuilder;
+import com.aspose.barcode.generation.BarCodeGenerator;
 import com.aspose.barcode.BarCodeImageFormat;
+import com.aspose.barcode.EncodeTypes;
 
 public class SaveBarcodeImageToStreams {
 
 	public static void main(String[] args) throws IOException {
     	
-		BarCodeBuilder builder = new BarCodeBuilder();
-                builder.setEncodeType(com.aspose.barcode.EncodeTypes.CODE_128);
-		builder.setCodeText("123456");
+		BarCodeGenerator generator = new BarCodeGenerator(EncodeTypes.CODE_128,"123456");
 		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-		builder.save(outStream, BarCodeImageFormat.JPEG);
-
+		generator.save(outStream, BarCodeImageFormat.JPEG);
 	}
-
 }
