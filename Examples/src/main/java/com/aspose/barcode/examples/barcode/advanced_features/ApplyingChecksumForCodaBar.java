ab50b2cbfc097e5d1974f25e11daa08bee766a57
muzammil-khan <muzammil.khan@aspose.com>
2019-03-08 22:15:28 +0500

    Update ApplyingChecksumForCodaBar.java

--- Examples/src/main/java/com/aspose/barcode/examples/barcode/advanced_features/ApplyingChecksumForCodaBar.java
+++ Examples/src/main/java/com/aspose/barcode/examples/barcode/advanced_features/ApplyingChecksumForCodaBar.java
@@ -1,59 +1,54 @@
 package com.aspose.barcode.examples.barcode.advanced_features;
 
+import java.io.IOException;
+
 import com.aspose.barcode.CodabarChecksumMode;
 import com.aspose.barcode.EnableChecksum;
 import com.aspose.barcode.barcoderecognition.BarCodeReader;
 import com.aspose.barcode.barcoderecognition.ChecksumValidation;
-import com.aspose.barcode.barcoderecognition.DecodeType;
 import com.aspose.barcode.examples.Utils;
+import com.aspose.barcode.generation.BarCodeGenerator;
 
 public class ApplyingChecksumForCodaBar 
 {
-	public static void main(String[] args) 
-        {
+	public static void main(String[] args) throws IOException 
+    {
 
 		// ExStart:ApplyingChecksumValidation
 
 		// The path to the resource directory.
 		String dataDir = Utils.getDataDir(ApplyingChecksumValidation.class) + "Barcode/AdvancedFeatures/";
 
-                //Generation
-                //Instantiate BarCodeBuilder object
-                com.aspose.barcode.BarCodeBuilder builder = new com.aspose.barcode.BarCodeBuilder();
+        //Generation
+        //Instantiate BarCodeGenerator object
+        BarCodeGenerator generator = new BarCodeGenerator(com.aspose.barcode.EncodeTypes.CODABAR, "1234567890");
         
-                //Set the Code text for the barcode
-                builder.setCodeText("1234567890");
+        //Set the EnableChecksum property to yes
+        generator.getChecksum().setEnable(EnableChecksum.YES);
         
-                //Set the symbology type to CodaBar
-                builder.setEncodeType(com.aspose.barcode.EncodeTypes.CODABAR);
+        //Set the CodabarChecksumMode
+        generator.getCodabar().setChecksumMode(CodabarChecksumMode.MOD_10);
         
-                //Set the EnableChecksum property to yes
-                builder.setEnableChecksum(EnableChecksum.YES);
-        
-                //Set the CodabarChecksumMode
-                builder.setCodabarChecksumMode(CodabarChecksumMode.MOD_10);
-        
-                //Save the image on the system
-                builder.save("Codabar_Mod10.png");
+        //Save the image on the system
+        generator.save("Codabar_Mod10.png");
 
-                //Recognition
-                //Initialize reader object
-                BarCodeReader reader = new BarCodeReader("Codabar_Mod10.png", com.aspose.barcode.barcoderecognition.DecodeType.CODABAR);
+        //Recognition
+        //Initialize reader object
+        BarCodeReader reader = new BarCodeReader("Codabar_Mod10.png", com.aspose.barcode.barcoderecognition.DecodeType.CODABAR);
         
-                //Set ChecksumValidation property of the reader to On
-                reader.setChecksumValidation(ChecksumValidation.ON);
-                while (reader.read())
-                {
-                     //Get code text
-                     System.out.println(" codetext: " + reader.getCodeText());
+        //Set ChecksumValidation property of the reader to On
+        reader.setChecksumValidation(ChecksumValidation.ON);
+        while (reader.read())
+        {
+        	//Get code text
+            System.out.println(" codetext: " + reader.getCodeText());
             
-                     //Get type of barcode
-                     System.out.println(" type: " + reader.getCodeType());
+            //Get type of barcode
+            System.out.println(" type: " + reader.getCodeType());
             
-                    //Get checksum value
-                    System.out.println(" Checksum: " + reader.getCheckSum());
-                }		
-
+            //Get checksum value
+            System.out.println(" Checksum: " + reader.getCheckSum());
+        }		
 		// ExEnd:ApplyingChecksumValidation
 	}
 }
