3ccd10008a588b1ac7d871f7b1e2a48b33c542f6
muzammil-khan <muzammil.khan@aoserv.com>
2019-08-03 15:02:44 +0500

    Create ProductInfo.java

---
+++ Examples/src/main/java/com/aspose/barcode/examples/ProductInfo.java
@@ -0,0 +1,15 @@
+package com.aspose.barcode.examples;
+
+import com.aspose.barcode.BuildVersionInfo;
+
+public class ProductInfo {
+
+	public static void main(String[] args) {
+		// ExStart: ProductInfo
+		System.out.println("Assembly version: " + BuildVersionInfo.ASSEMBLY_VERSION);
+		System.out.println("Product: " + BuildVersionInfo.PRODUCT);
+		System.out.println("Release Date: " + BuildVersionInfo.RELEASE_DATE);
+		// ExEnd: ProductInfo
+	}
+
+}
