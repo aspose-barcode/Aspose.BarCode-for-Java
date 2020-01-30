010bd833be65a7655e0b4abe306766f5713d3aa1
muzammil-khan <muzammil.khan@aoserv.com>
2020-01-30 14:33:29 +0500

    Updates for Aspose.BarCode for Java 20.1

---
+++ Examples/src/main/java/com/aspose/barcode/examples/GenerateComplexBarcodes/GenerateSwissQR.java
@@ -0,0 +1,35 @@
+package com.aspose.barcode.examples.GenerateComplexBarcodes;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import com.aspose.barcode.BarCodeImageFormat;
+import com.aspose.barcode.complexbarcode.ComplexBarcodeGenerator;
+import com.aspose.barcode.complexbarcode.SwissQRCodetext;
+import com.aspose.barcode.examples.Utils;
+
+public class GenerateSwissQR {
+
+	public static void main(String[] args) throws IOException {
+		// ExStart:GenerateComplexBarcodes
+        // The path to the documents directory.
+		String dataDir = Utils.getDataDir(GenerateSwissQR.class) + "GenerateSwissQR/";
+
+        // Instantiate barcode object and set CodeText
+		SwissQRCodetext swissQRCodetext = new SwissQRCodetext();
+        swissQRCodetext.getBill().setAccount("Account");
+        swissQRCodetext.getBill().setBillInformation("BillInformation");
+        swissQRCodetext.getBill().setCurrency("EUR");
+
+        ComplexBarcodeGenerator generator = new ComplexBarcodeGenerator(swissQRCodetext);
+        BufferedImage img = generator.generateBarCodeImage();
+
+        generator.getParameters().getBarcode().getCodeTextParameters().setTwoDDisplayText("test");
+
+        // Save the Barcode image in JPEG format
+        generator.save(dataDir + "swissQRCodetext_out.png", BarCodeImageFormat.PNG);
+        // ExEnd:GenerateComplexBarcodes
+        System.out.println("Barcode saved at " + dataDir);
+	}
+
+}
---
+++ Examples/src/main/java/com/aspose/barcode/examples/GenerateComplexBarcodes/ReadSwissQR.java
@@ -0,0 +1,26 @@
+package com.aspose.barcode.examples.GenerateComplexBarcodes;
+
+import com.aspose.barcode.barcoderecognition.BarCodeReader;
+import com.aspose.barcode.barcoderecognition.BarCodeResult;
+import com.aspose.barcode.barcoderecognition.DecodeType;
+import com.aspose.barcode.complexbarcode.ComplexCodetextReader;
+import com.aspose.barcode.complexbarcode.SwissQRCodetext;
+import com.aspose.barcode.examples.Utils;
+
+public class ReadSwissQR {
+
+	public static void main(String[] args) {
+		// ExStart:ReadComplexBarcodes
+        // The path to the documents directory.
+		String dataDir = Utils.getDataDir(ReadSwissQR.class) + "ReadSwissQR/";
+ 
+		BarCodeReader reader = new BarCodeReader(dataDir + "swissQRCodetext_out.png", DecodeType.QR);
+		for (BarCodeResult barcodeResult : reader.readBarCodes()){
+			SwissQRCodetext result = ComplexCodetextReader.tryDecodeSwissQR(barcodeResult.getCodeText());
+			System.out.println("Account :" + result.getBill().getAccount());
+			System.out.println("BillInformation = " + result.getBill().getBillInformation());
+			System.out.println("Currency :" + result.getBill().getCurrency());
+        }
+        // ExEnd:ReadComplexBarcodes
+	}
+}
