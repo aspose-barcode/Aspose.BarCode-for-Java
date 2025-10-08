ace849382208ee5b2e1bb8593d201753ad9a064a
Aleksander Grinin <Aleksander.Grinin@aspose.com>
2025-10-08 17:58:57 +0300

    Add ExampleAssist

--- src/test/java/com/aspose/barcode/guide/common/ExampleAssist.java
+++ src/test/java/com/aspose/barcode/guide/common/ExampleAssist.java
@@ -3,6 +3,7 @@ package com.aspose.barcode.guide.common;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ExampleAssist
@@ -11,6 +12,28 @@ public class ExampleAssist
     {
         // Utility class, не должна создаваться
         throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static String getResourceFolderPath(String... pathParts)
+    {
+        Path basePath = Paths.get("src", "test", "resources");
+        for (String part : pathParts)
+        {
+            basePath = basePath.resolve(part);
+        }
+        return basePath.toString() + File.separator;
+    }
+
+    public static String getResourceFilePath(String... pathParts)
+    {
+        Path basePath = Paths.get("src", "test", "resources");
+
+        for (String part : pathParts)
+        {
+            basePath = basePath.resolve(part);
+        }
+
+        return basePath.toString();
     }
 
     public static String getResourcePath(String resourcePath)
@@ -25,17 +48,26 @@ public class ExampleAssist
         return resource.getPath();
     }
 
-    public static InputStream getResourceAsStream(String resourcePath)
-    {
-        InputStream stream = ExampleAssist.class.getClassLoader()
-                .getResourceAsStream(resourcePath);
-
-        if (stream == null) {
-            throw new IllegalArgumentException("Resource not found: " + resourcePath);
+    public static InputStream getResourceAsStream(String path) {
+        ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+        InputStream in = (tccl != null) ? tccl.getResourceAsStream(path) : null;
+        if (in == null) {
+            in = ExampleAssist.class.getClassLoader().getResourceAsStream(path);
         }
-
-        return stream;
+        return in;
     }
+
+//    public static InputStream getResourceAsStream(String resourcePath)
+//    {
+//        InputStream stream = ExampleAssist.class.getClassLoader()
+//                .getResourceAsStream(resourcePath);
+//
+//        if (stream == null) {
+//            throw new IllegalArgumentException("Resource not found: " + resourcePath);
+//        }
+//
+//        return stream;
+//    }
 
     public static File getResourceFile(String resourcePath) throws Exception
     {
